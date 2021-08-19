68816ed5494a1b8b526d73af91222bd89058f783
LLLgoyour <lllgoyour@protonmail.com>
2021-08-19 02:07:10 -0400

    1234

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # XiaoBing
 change again
 现在修改了用那玩意提交
+123
--- push.sh
+++ push.sh
@@ -3,8 +3,8 @@ git add .
 echo "请输入您对本次提交修改的描述："
 read remark
 git commit -a -m "${remark}"
-echo "正在拉取远端代码，您可能需要手动合并冲突（如果有）...\n"
+echo "正在拉取远端代码，您可能需要手动合并冲突（如果有）..."
 git pull origin master
-echo "正在将本地修改提交到远端...\n"
+echo "正在将本地修改提交到远端..."
 git push origin master
 echo "---操作完成---"
